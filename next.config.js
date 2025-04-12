f3bd5a461666f93745364c1892cb8535b85671cb
Shuhei Hasegawa <shuhei.hasegawa1989@gmail.com>
2025-04-12 11:03:25 +0900

    feat: package.jsonの更新

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "css")],
@@ -20,6 +21,25 @@ module.exports = {
         fs: false,
       };
     }
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
     return config;
   },
+  env: {
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/ir/:path*",
+        destination: "/news/ir/:path*",
+        permanent: true, // 301リダイレクト（恒久的）
+      },
+      // 必要に応じて他のリダイレクトも追加
+    ];
+  },
 };
+
+module.exports = nextConfig;
